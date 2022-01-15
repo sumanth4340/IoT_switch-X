f720a2c334e736d3d4d05047d7af959d98a24503
Sumanth.x_ <76698251+sumanth4340@users.noreply.github.com>
2022-01-15 12:38:14 +0530

    Update Iot_SWitch.ino

--- Iot_SWitch/Iot_SWitch.ino
+++ Iot_SWitch/Iot_SWitch.ino
@@ -1,112 +1,252 @@
+/**********************************************************************************
+ *  Preferences--> Aditional boards Manager URLs : 
+ *  https://dl.espressif.com/dl/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
+ *  
+ *  Download Board ESP8266 NodeMCU : https://github.com/esp8266/Arduino
+ *  Download the libraries
+ *  Blynk 1.0.0-beta.3 Library:  https://blynk.io/
+ *  AceButton Library:  https://github.com/bxparks/AceButton
+ **********************************************************************************/
+
+// Fill-in information from your Blynk Template here
 #define BLYNK_TEMPLATE_ID "TMPLg0EUxVS-"
 #define BLYNK_DEVICE_NAME "Switch iot X"
+
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
+
 #define BLYNK_PRINT Serial
-#define BLYNK_DEBUG
-#define APP_DEBUG
+//#define BLYNK_DEBUG
 
-bool toggleState_1 = LOW;
-bool toggleState_2 = LOW; 
-bool toggleState_3 = LOW; 
-bool toggleState_4 = LOW; 
+//#define APP_DEBUG
 
-bool SwitchState_1 = LOW;
-bool SwitchState_2 = LOW;
-bool SwitchState_3 = LOW;
-bool SwitchState_4 = LOW;
+// Uncomment your board, or configure a custom board in Settings.h
+//#define USE_SPARKFUN_BLYNK_BOARD
+#define USE_NODE_MCU_BOARD
+//#define USE_WITTY_CLOUD_BOARD
 
 #include "BlynkEdgent.h"
+#include <AceButton.h>
+using namespace ace_button;
+
+// define the GPIO connected with Relays and switches
+#define RelayPin1 5  //D1
+#define RelayPin2 4  //D2
+#define RelayPin3 14 //D5
+#define RelayPin4 12 //D6
+
+#define SwitchPin1 10  //SD3
+#define SwitchPin2 D3   //D3 
+#define SwitchPin3 13  //D7
+#define SwitchPin4 3   //RX
+
+#define wifiLed   16   //D0
+
+//Change the virtual pins according the rooms
+#define VPIN_BUTTON_1    V1
+#define VPIN_BUTTON_2    V2
+#define VPIN_BUTTON_3    V3 
+#define VPIN_BUTTON_4    V4
+
+int toggleState_1 = 0; //Define integer to remember the toggle state for relay 1
+int toggleState_2 = 0; //Define integer to remember the toggle state for relay 2
+int toggleState_3 = 0; //Define integer to remember the toggle state for relay 3
+int toggleState_4 = 0; //Define integer to remember the toggle state for relay 4
+
+ButtonConfig config1;
+AceButton button1(&config1);
+ButtonConfig config2;
+AceButton button2(&config2);
+ButtonConfig config3;
+AceButton button3(&config3);
+ButtonConfig config4;
+AceButton button4(&config4);
+
+void handleEvent1(AceButton*, uint8_t, uint8_t);
+void handleEvent2(AceButton*, uint8_t, uint8_t);
+void handleEvent3(AceButton*, uint8_t, uint8_t);
+void handleEvent4(AceButton*, uint8_t, uint8_t);
+
+void relayOnOff(int relay){
+    switch(relay){
+      case 1: 
+             if(toggleState_1 == 0){
+              digitalWrite(RelayPin1, LOW); // turn on relay 1
+              toggleState_1 = 1;
+              Serial.println("Device1 ON");
+              }
+             else{
+              digitalWrite(RelayPin1, HIGH); // turn off relay 1
+              toggleState_1 = 0;
+              Serial.println("Device1 OFF");
+              }
+             delay(100);
+      break;
+      case 2: 
+             if(toggleState_2 == 0){
+              digitalWrite(RelayPin2, LOW); // turn on relay 2
+              toggleState_2 = 1;
+              Serial.println("Device2 ON");
+              }
+             else{
+              digitalWrite(RelayPin2, HIGH); // turn off relay 2
+              toggleState_2 = 0;
+              Serial.println("Device2 OFF");
+              }
+             delay(100);
+      break;
+      case 3: 
+             if(toggleState_3 == 0){
+              digitalWrite(RelayPin3, LOW); // turn on relay 3
+              toggleState_3 = 1;
+              Serial.println("Device3 ON");
+              }
+             else{
+              digitalWrite(RelayPin3, HIGH); // turn off relay 3
+              toggleState_3 = 0;
+              Serial.println("Device3 OFF");
+              }
+             delay(100);
+      break;
+      case 4: 
+             if(toggleState_4 == 0){
+              digitalWrite(RelayPin4, LOW); // turn on relay 4
+              toggleState_4 = 1;
+              Serial.println("Device4 ON");
+              }
+             else{
+              digitalWrite(RelayPin4, HIGH); // turn off relay 4
+              toggleState_4 = 0;
+              Serial.println("Device4 OFF");
+              }
+             delay(100);
+      break;
+      default : break;      
+      }  
+}
 
 BLYNK_CONNECTED() {
-  Blynk.syncVirtual(V0);
-  Blynk.syncVirtual(V1);
-  Blynk.syncVirtual(V2);
-  Blynk.syncVirtual(V3);
+  // Request the latest state from the server
+  Blynk.syncVirtual(VPIN_BUTTON_1);
+  Blynk.syncVirtual(VPIN_BUTTON_2);
+  Blynk.syncVirtual(VPIN_BUTTON_3);
+  Blynk.syncVirtual(VPIN_BUTTON_4);
 }
 
-BLYNK_WRITE(V0) {
+// When App button is pushed - switch the state
+
+BLYNK_WRITE(VPIN_BUTTON_1) {
   toggleState_1 = param.asInt();
   if(toggleState_1 == 1){
-    digitalWrite(D5, LOW);
+    digitalWrite(RelayPin1, LOW);
   }
   else { 
-    digitalWrite(D5, HIGH);
+    digitalWrite(RelayPin1, HIGH);
   }
 }
 
-BLYNK_WRITE(V1) {
+BLYNK_WRITE(VPIN_BUTTON_2) {
   toggleState_2 = param.asInt();
   if(toggleState_2 == 1){
-    digitalWrite(D1, LOW);
+    digitalWrite(RelayPin2, LOW);
   }
   else { 
-    digitalWrite(D1, HIGH);
+    digitalWrite(RelayPin2, HIGH);
   }
 }
 
-BLYNK_WRITE(V2) {
+BLYNK_WRITE(VPIN_BUTTON_3) {
   toggleState_3 = param.asInt();
   if(toggleState_3 == 1){
-    digitalWrite(D2, LOW);
+    digitalWrite(RelayPin3, LOW);
   }
   else { 
-    digitalWrite(D2, HIGH);
+    digitalWrite(RelayPin3, HIGH);
   }
 }
 
-BLYNK_WRITE(V3) {
+BLYNK_WRITE(VPIN_BUTTON_4) {
   toggleState_4 = param.asInt();
   if(toggleState_4 == 1){
-    digitalWrite(D8, LOW);
+    digitalWrite(RelayPin4, LOW);
   }
   else { 
-    digitalWrite(D8, HIGH);
+    digitalWrite(RelayPin4, HIGH);
   }
-}
-
-BLYNK_WRITE(V9) {
-  all_SwitchOff();
-}
-
-
-void all_SwitchOff(){
-  toggleState_1 = 0; digitalWrite(D5, HIGH); Blynk.virtualWrite(V0, toggleState_1); delay(100);
-  toggleState_2 = 0; digitalWrite(D1, HIGH); Blynk.virtualWrite(V1, toggleState_2); delay(100);
-  toggleState_3 = 0; digitalWrite(D2, HIGH); Blynk.virtualWrite(V2, toggleState_3); delay(100);
-  toggleState_4 = 0; digitalWrite(D8, HIGH); Blynk.virtualWrite(V3, toggleState_4); delay(100);
 }
 
 void setup()
 {
   Serial.begin(115200);
-
-  pinMode(D5, OUTPUT);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D8, OUTPUT);
-
+  delay(100);
+  
+  pinMode(RelayPin1, OUTPUT);
+  pinMode(RelayPin2, OUTPUT);
+  pinMode(RelayPin3, OUTPUT);
+  pinMode(RelayPin4, OUTPUT);
 
   pinMode(wifiLed, OUTPUT);
 
-  pinMode(D4, INPUT_PULLUP);
-  pinMode(D3, INPUT_PULLUP);
-  pinMode(D7, INPUT_PULLUP);
-  pinMode(D6, INPUT_PULLUP);
+  pinMode(SwitchPin1, INPUT_PULLUP);
+  pinMode(SwitchPin2, INPUT_PULLUP);
+  pinMode(SwitchPin3, INPUT_PULLUP);
+  pinMode(SwitchPin4, INPUT_PULLUP);
 
-  
+  //During Starting all Relays should TURN OFF
+  digitalWrite(RelayPin1, HIGH);
+  digitalWrite(RelayPin2, HIGH);
+  digitalWrite(RelayPin3, HIGH);
+  digitalWrite(RelayPin4, HIGH);
 
-  digitalWrite(D5, HIGH);
-  digitalWrite(D1, HIGH);
-  digitalWrite(D2, HIGH);
-  digitalWrite(D8, HIGH);
+  digitalWrite(wifiLed, HIGH);
 
+  config1.setEventHandler(button1Handler);
+  config2.setEventHandler(button2Handler);
+  config3.setEventHandler(button3Handler);
+  config4.setEventHandler(button4Handler);
+
+  button1.init(SwitchPin1);
+  button2.init(SwitchPin2);
+  button3.init(SwitchPin3);
+  button4.init(SwitchPin4);
 
   BlynkEdgent.begin();
+
+  Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1);
+  Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2);
+  Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3);
+  Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4);
+
+  //timer.setInterval(3000L, checkBlynkStatus); // check if Blynk server is connected every 3 seconds
 }
 
 void loop() {
 
     BlynkEdgent.run();
-    
-    manual_control(); 
+
+    //Manual Switch Control
+    button1.check();
+    button2.check();
+    button3.check();
+    button4.check();
+}
+
+void button1Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+  Serial.println("EVENT1");
+  relayOnOff(1);
+  Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1);   // Update Button Widget
+}
+void button2Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+  Serial.println("EVENT2");
+  relayOnOff(2);
+  Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2);   // Update Button Widget
+}
+void button3Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+  Serial.println("EVENT3");
+  relayOnOff(3);
+  Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3);   // Update Button Widget
+}
+void button4Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+  Serial.println("EVENT4");
+  relayOnOff(4);
+  Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4);   // Update Button Widget
 }
